c6ec24c991488504ebc8ec61e2eec91565b2b43f
Brian Grey <mixolyde@gmail.com>
2015-09-12 18:21:07 -0400

    Start js file with form functions

---
+++ hmwk2/happy.js
@@ -0,0 +1,71 @@
+
+//return state codes for drop down
+function stateOptions() {
+  document.write(`<option value="AK">AK</option>
+  <option value="AL">AL</option>
+  <option value="AR">AR</option>
+  <option value="AZ">AZ</option>
+  <option value="CA">CA</option>
+  <option value="CO">CO</option>
+  <option value="CT">CT</option>
+  <option value="DC">DC</option>
+  <option value="DE">DE</option>
+  <option value="FL">FL</option>
+  <option value="GA">GA</option>
+  <option value="HI">HI</option>
+  <option value="IA">IA</option>
+  <option value="ID">ID</option>
+  <option value="IL">IL</option>
+  <option value="IN">IN</option>
+  <option value="KS">KS</option>
+  <option value="KY">KY</option>
+  <option value="LA">LA</option>
+  <option value="MA">MA</option>
+  <option value="MD">MD</option>
+  <option value="ME">ME</option>
+  <option value="MI">MI</option>
+  <option value="MN">MN</option>
+  <option value="MO">MO</option>
+  <option value="MS">MS</option>
+  <option value="MT">MT</option>
+  <option value="NC">NC</option>
+  <option value="ND">ND</option>
+  <option value="NE">NE</option>
+  <option value="NH">NH</option>
+  <option value="NJ">NJ</option>
+  <option value="NM">NM</option>
+  <option value="NV">NV</option>
+  <option value="NY">NY</option>
+  <option value="OH">OH</option>
+  <option value="OK">OK</option>
+  <option value="OR">OR</option>
+  <option value="PA">PA</option>
+  <option value="RI">RI</option>
+  <option value="SC">SC</option>
+  <option value="SD">SD</option>
+  <option value="TN">TN</option>
+  <option value="TX">TX</option>
+  <option value="UT">UT</option>
+  <option value="VA">VA</option>
+  <option value="VT">VT</option>
+  <option value="WA">WA</option>
+  <option value="WI">WI</option>
+  <option value="WV">WV</option>
+  <option value="WY">WY</option>`);
+}
+
+function monthOptions() {
+  document.write(`
+	  <option value="1">January</option>
+	  <option value="2">February</option>
+	  <option value="3">March</option>
+	  <option value="4">April</option>
+	  <option value="5">May</option>
+	  <option value="6">June</option>
+	  <option value="7">July</option>
+	  <option value="8">August</option>
+	  <option value="9">September</option>
+	  <option value="10">October</option>
+	  <option value="11">November</option>
+	  <option value="12">December</option> `);
+}
