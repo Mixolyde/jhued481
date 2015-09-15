1ca2928bf83a991d1fb0a6884faba554c9ae5ebc
Brian Grey <mixolyde@gmail.com>
2015-09-15 17:30:10 -0400

    Fix var name

--- hmwk2/happy.js
+++ hmwk2/happy.js
@@ -1,5 +1,5 @@
 var querydict = {};
-location.search.substr(1).split("&").forEach(function(item) {var s = item.split("="), k = s[0], v = s[1] && decodeURIComponent(s[1]); (k in qd) ? qd[k].push(v) : qd[k] = [v]})
+location.search.substr(1).split("&").forEach(function(item) {var s = item.split("="), k = s[0], v = s[1] && decodeURIComponent(s[1]); (k in querydict) ? querydict[k].push(v) : querydict[k] = [v]})
 
 function printQueryDict(){
   for (k in querydict)
