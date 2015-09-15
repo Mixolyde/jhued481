3e596899aca2ae00e4011d69be80b55f10be3243
Brian Grey <mixolyde@gmail.com>
2015-09-15 17:11:59 -0400

    Add query dict

--- hmwk2/happy.js
+++ hmwk2/happy.js
@@ -1,3 +1,5 @@
+var querydict = {};
+location.search.substr(1).split("&").forEach(function(item) {var s = item.split("="), k = s[0], v = s[1] && decodeURIComponent(s[1]); (k in qd) ? qd[k].push(v) : qd[k] = [v]})
 
 //return state codes for drop down
 function stateOptions() {
