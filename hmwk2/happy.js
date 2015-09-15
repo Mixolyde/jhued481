ec57e3b5740a24208f448a2e0a80ebc6d9039e21
Brian Grey <mixolyde@gmail.com>
2015-09-15 17:22:11 -0400

    Add print query function

--- hmwk2/happy.js
+++ hmwk2/happy.js
@@ -1,6 +1,12 @@
 var querydict = {};
 location.search.substr(1).split("&").forEach(function(item) {var s = item.split("="), k = s[0], v = s[1] && decodeURIComponent(s[1]); (k in qd) ? qd[k].push(v) : qd[k] = [v]})
 
+function printQueryDict(){
+  for (k in querydict)
+  {
+    document.write(k + "="+dico[k]+"<br/>\n");
+  }
+}
 //return state codes for drop down
 function stateOptions() {
   document.write(`<option value="AK">AK</option>
