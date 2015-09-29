319ac1ed4d3330de91ea54f010feac40f08c5466
Brian Grey <mixolyde@gmail.com>
2015-09-28 23:23:49 -0400

    Start google map and geo location

--- redx/scripts/redx.js
+++ redx/scripts/redx.js
@@ -2,6 +2,9 @@
 
 //globals
 var sections;
+var userPosition;
+var map;
+
 //init globals
 function initPage(){
   sections = [
@@ -10,15 +13,48 @@ function initPage(){
     document.getElementById("locations_section"),
     document.getElementById("media_section"),
     document.getElementById("contact_section"),
-    ]
+    ];
 
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        setLocation, fail, {timeout:10000}); 
+    } else {
+      fail();
+    }
+
+    var mapOptions = {    
+      center: new google.maps.LatLng(38.898013, -77.036539),
+      zoom: 9 }; 
+    map = new google.maps.Map(
+      document.getElementById("locationMap"),
+      mapOptions); 
+    initMap();
+}
+
+function initMap(){
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(38.898013, -77.036539),
+    map: map,
+    title: "Our Locations"
+    });
 }
 
 function showSection(name){
   sections.forEach(function (item, index, array) {
-      item.style.display = "none";
-      if(item.id.indexOf(name) == 0){
-        item.style.display = "";
-      }
+    item.style.display = "none";
+    if(item.id.indexOf(name) == 0){
+      item.style.display = "";
+    }
   });
 }
+
+function setLocation(position) {
+  console.log("Longitude: " + position.coords.longitude);
+  console.log("Latitude: " + position.coords.latitude);
+  userPosition = position;
+}
+
+function fail() {
+  console.log("Geolocation information not available or not authorized.");
+  userPosition = null;
+} 
