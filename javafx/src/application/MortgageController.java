0370ec320a2371731341259bd882c6e1933d758e
Brian Grey <mixolyde@gmail.com>
2015-10-25 20:45:29 -0400

    Monthly payment calculations except last

--- javafx/src/application/MortgageController.java
+++ javafx/src/application/MortgageController.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
@@ -12,6 +13,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class MortgageController implements Initializable {
 	public TextField loanAmountField;
@@ -46,18 +48,80 @@ public class MortgageController implements Initializable {
 		yearBox.setItems(yearOptions);
 		yearBox.getSelectionModel().selectFirst();
 		
-		//TODO set cell value factories
+		monthNoCol.setCellValueFactory(
+			new PropertyValueFactory<MortgagePayment,String>("monthNo")
+		);
 		
+		paymentCol.setCellValueFactory(
+			new PropertyValueFactory<MortgagePayment,Double>("payment")
+		);
 		
+		principlePaidCol.setCellValueFactory(
+			new PropertyValueFactory<MortgagePayment,Double>("principlePaid")
+		);
 		
+		interestPaidCol.setCellValueFactory(
+			new PropertyValueFactory<MortgagePayment,Double>("interestPaid")
+		);
 		
-		payments = FXCollections.observableArrayList(new MortgagePayment("Month 1", 1, 2, 3, 4, 5));
+		totalInterestPaidCol.setCellValueFactory(
+			new PropertyValueFactory<MortgagePayment,Double>("totalInterestPaid")
+		);
+		
+		remainingValueCol.setCellValueFactory(
+			new PropertyValueFactory<MortgagePayment,Double>("remainingValue")
+		);
+		
+		payments = FXCollections.observableArrayList();
 		paymentTable.setItems(payments);
 		
 	}
 	
 	public void handleCalculateAction(ActionEvent event){
 		System.out.println("You clicked the Calculate Button!");
+		double interestMonthly = interestBox.getValue() / 12 / 100;
+		System.out.println("Monthly interest percentage: " + interestMonthly);
+		
+		int numberMonths = 12 * yearBox.getValue();
+		int index = 1;
+		
+		payments = FXCollections.observableArrayList();
+		double totalPaid = 0;
+		double totalInterestPaid = 0;
+		
+		try {
+			String loanAmountString = loanAmountField.getText();
+			double remainingBalance = Double.parseDouble(loanAmountString);
+			System.out.println("Initial principal: " + remainingBalance);
+			double monthlyPayment = remainingBalance * interestMonthly * 
+					Math.pow(1 + interestMonthly, (double) numberMonths) /
+					(Math.pow(1 + interestMonthly, (double) numberMonths) - 1);
+			DecimalFormat df = new DecimalFormat("#.00");
+			monthlyPayment = Double.parseDouble(df.format(monthlyPayment));
+			System.out.println("Monthly Payment: " + monthlyPayment);
+			
+			for (index = 1; index < numberMonths; index++){
+				double interestPaid = remainingBalance * interestMonthly;
+				double principlePaid = monthlyPayment - interestPaid;
+				remainingBalance = remainingBalance - monthlyPayment;
+				totalInterestPaid += interestPaid;
+				totalPaid += monthlyPayment;
+				payments.add(new MortgagePayment("Month " + index, monthlyPayment,
+						principlePaid, interestPaid,
+						totalInterestPaid, remainingBalance));
+				
+			}
+			
+			totalField.setText(df.format(totalPaid));
+			
+		} catch(Exception e){
+			System.out.println("Exception thrown while calculating: " + e.getMessage());
+			e.printStackTrace();
+			totalField.setText("");
+			
+		}
+		
+		
 	}
 
 
