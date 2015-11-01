baecce59d6aca4cb0016499bb56e36e00e68adb7
Brian Grey <mixolyde@gmail.com>
2015-11-01 10:26:13 -0500

    Menu actions

--- javafx/src/application/RetailSystemController.java
+++ javafx/src/application/RetailSystemController.java
@@ -28,12 +28,42 @@ public class RetailSystemController implements Initializable {
 		
 		System.out.println("Printing database dump.");
 		try {
-		DerbyManager.getInstance().printData();
+			DerbyManager.getInstance().printData();
 		} catch (Exception e){
 			e.printStackTrace();
 		}
 		
 		System.exit(0);
+	}
+	
+	public void handleCustomerAddAction(ActionEvent event){
+		System.out.println("Handling Customer Add Action");
+		
+	}
+	
+	public void handleCustomerListAction(ActionEvent event){
+		System.out.println("Handling Customer List Action");
+		
+	}
+	
+	public void handleEmployeeAddAction(ActionEvent event){
+		System.out.println("Handling Employee Add Action");
+		
+	}
+	
+	public void handleEmployeeListAction(ActionEvent event){
+		System.out.println("Handling Employee List Action");
+		
+	}
+	
+	public void handleMerchandiseAddAction(ActionEvent event){
+		System.out.println("Handling Merchandise Add Action");
+		
+	}
+	
+	public void handleMerchandiseListAction(ActionEvent event){
+		System.out.println("Handling Merchandise List Action");
+		
 	}
 
 }
